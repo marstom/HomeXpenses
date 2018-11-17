eec47994b38a3abe6557b4afb3fb99fbd9e1b298
Tomasz Marszałek <tomasz.marszalek@idego.pl>
2018-11-17 11:21:38 +0100

    Clean home vue

--- xpenses_vue/src/views/Home.vue
+++ xpenses_vue/src/views/Home.vue
@@ -14,16 +14,6 @@
     </label>
   </div>
 
-
-  <div>
-    <label for="">Wszystkie
-      <input type="radio" name="a" id="" value="expense.json" v-model="pick" @change="getData">
-    </label>
-    <label for="">Zwinięte
-      <input type="radio" name="a" id="" value="expense_zipped.json" v-model="pick" @change="getData">
-    </label>
-  </div>
-
       <table class="table table-bordered">
         <thead>
           <tr>
@@ -49,8 +39,6 @@
 
   <div class="col-12">
     <button class="btn btn-warning" @click="saveAllChanges">Save all changes</button>
-    <button class="btn btn-primary" @click="undoAllChanges">Undo</button>
-    <button class="btn btn-secondary" @click="zip">Zwiń po kategorii</button>
   </div>
       <price-form @sentit="getData"></price-form>
   </div>
@@ -87,51 +75,30 @@ export default {
       ],
       originalData: [], // for backup
       current: '',
-      pick: 'expense.json'
+      pick: '/expenses_list'
     }
   },
   methods:{
     getData(){
-      axios.get(this.pick)
-        .then(response => {
-          this.tableItems = response.data
-          // const copy = JSON.parse(JSON.stringify(response.data))
-          this.originalData = JSON.parse(JSON.stringify(response.data))
-        })
-         .catch(error => {
-           console.log(error)
-         })
+      // axios.get('/expenses_list')
+      //   .then(response => {
+      //     this.tableItems = response.data
+      //     this.originalData = JSON.parse(JSON.stringify(response.data))
+      //   })
+      //    .catch(error => {
+      //      console.log(error)
+      //    })
     },
+
     remove(it, key){
-      let dic = {}
-      dic[key] = null
-      axios.patch(this.pick, dic)
-        .then(response => {
-          console.log(response);
-          this.getData()
-        })
-
     },
+
     saveAllChanges(){
-      // let dic = {}
-      // dic[key] = it
-      // dic[key].name = 'test'
-      for(let key in this.tableItems){
-        this.tableItems[key].name.e = false
-        this.tableItems[key].price.e = false
-        this.tableItems[key].category.e = false
-      }
-      axios.patch(this.pick, this.tableItems)
-        .then(response => {
-          console.log(response);
-          this.getData()
-        })
     },
-    undoAllChanges(){
-      this.tableItems = JSON.parse(JSON.stringify(this.originalData)) //not assign pointer but data
-      // this.getData()
 
+    undoAllChanges(){
     },
+
     editedInput(e, key, typ){
       if(typ === 'name'){
         this.tableItems[key].name.v = e.target.value
@@ -147,56 +114,8 @@ export default {
       }
 
     },
-    zip(){
-
-      const categorySum = {}
-      const keysToDelete = []
-      // extract categories
-      for(let key in this.tableItems){
-        let categoryName = this.tableItems[key].category.v
-        let categoryPrice = this.tableItems[key].price.v
-        if(categorySum[categoryName] === undefined){
-          categorySum[categoryName] = 0
-        }
-        categorySum[categoryName] += categoryPrice
-        keysToDelete.push(key)
-      }
-
-      /// post zipped entrys
-      const entrysList = createZippedEntrys(categorySum)
-        axios.put('expense_zipped.json', {})
-          .then(response =>{
-            entrysList.forEach(ent => {
-              console.log(ent);
-              axios.post('expense_zipped.json', ent)
-            })
-          })
-    }
   }
 }
-
-
-const deleteKeys = (keys) => {
-  const entrys = {}
-  keys.forEach(element => {
-    entrys[element] = null
-  });
-  return entrys
-}
-
-/// creates zipped entrys list 
-const createZippedEntrys = (categories) => {
-  const entrys = []
-  for(let key in categories){
-      entrys.push({
-          name: { v:'-', e:false },
-          price: { v:categories[key], e:false },
-          category: { v:key, e:false },
-      })
-  }
-  return entrys
-}
-
 </script>
 
 <style>
